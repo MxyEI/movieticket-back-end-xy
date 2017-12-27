f58c7a23e6241e98ab5d3ebfcbddc77a1be14268
MxyEI <1486157956@qq.com>
2017-12-27 17:55:40 +0800

    使用restful风格API

--- src/main/java/com/company/project/web/UsersController.java
+++ src/main/java/com/company/project/web/UsersController.java
@@ -1,14 +1,12 @@
 package com.company.project.web;
+
 import com.company.project.core.Result;
 import com.company.project.core.ResultGenerator;
 import com.company.project.model.Users;
 import com.company.project.service.UsersService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -22,31 +20,31 @@ public class UsersController {
     @Resource
     private UsersService usersService;
 
-    @PostMapping("/add")
-    public Result add(Users users) {
+    @PostMapping
+    public Result add(@RequestBody Users users) {
         usersService.save(users);
         return ResultGenerator.genSuccessResult();
     }
 
-    @PostMapping("/delete")
-    public Result delete(@RequestParam Integer id) {
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Integer id) {
         usersService.deleteById(id);
         return ResultGenerator.genSuccessResult();
     }
 
-    @PostMapping("/update")
-    public Result update(Users users) {
+    @PutMapping
+    public Result update(@RequestBody Users users) {
         usersService.update(users);
         return ResultGenerator.genSuccessResult();
     }
 
-    @PostMapping("/detail")
-    public Result detail(@RequestParam Integer id) {
+    @GetMapping("/{id}")
+    public Result detail(@PathVariable Integer id) {
         Users users = usersService.findById(id);
         return ResultGenerator.genSuccessResult(users);
     }
 
-    @PostMapping("/list")
+    @GetMapping
     public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size) {
         PageHelper.startPage(page, size);
         List<Users> list = usersService.findAll();
