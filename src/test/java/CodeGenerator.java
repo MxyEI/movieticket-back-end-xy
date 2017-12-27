33687b1b351335924bd2f9bb2e0323ce1eea09d3
MxyEI <1486157956@qq.com>
2017-12-27 19:53:41 +0800

    更新数据库连接信息

--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -18,7 +18,7 @@ import static com.company.project.core.ProjectConstant.*;
  */
 public class CodeGenerator {
     //JDBC配置，请修改为你项目的实际配置
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/monkey?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/movieticket?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull";
     private static final String JDBC_USERNAME = "root";
     private static final String JDBC_PASSWORD = "mxy123";
     private static final String JDBC_DIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
