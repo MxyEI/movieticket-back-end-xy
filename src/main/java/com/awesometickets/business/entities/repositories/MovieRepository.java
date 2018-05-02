cb7c51ecdd8c43f1403b78345cd9b724881c6067
MxyEI <1486157956@qq.com>
2018-05-02 14:11:54 +0800

    电影信息的映射类

--- src/main/java/com/awesometickets/business/entities/repositories/MovieRepository.java
+++ src/main/java/com/awesometickets/business/entities/repositories/MovieRepository.java
@@ -11,12 +11,15 @@ import java.util.List;
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
 
+    //查询电影是否在售
     @Query("select m.movieId from Movie m where m.movieStatus.statusName = ?1")
     List<Integer> findByMovieStatus(String status);
 
+    //查询首页大图
     @Query("select m.movieId, m.posterLarge from Movie m where m.posterLarge is not null AND m.posterLarge !=''")
     List<Object[]> findLargePoster();
 
+    //根据电影ID查看详情
     @Query("from Movie m " +
             "left outer join fetch m.country " +
             "left outer join fetch m.movieStatus " +
