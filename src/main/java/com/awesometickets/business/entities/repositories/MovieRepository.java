f0fdea53fd9c4600d7db68ebde5b858b9c2d969e
MxyEI <1486157956@qq.com>
2018-05-04 12:43:36 +0800

    fetch及时加载

--- src/main/java/com/awesometickets/business/entities/repositories/MovieRepository.java
+++ src/main/java/com/awesometickets/business/entities/repositories/MovieRepository.java
@@ -19,7 +19,7 @@ public interface MovieRepository extends CrudRepository<Movie, Integer> {
     @Query("select m.movieId, m.posterLarge from Movie m where m.posterLarge is not null AND m.posterLarge !=''")
     List<Object[]> findLargePoster();
 
-    //根据电影ID查看详情
+    //根据电影ID查看详情 hibernate 的left join fetch可以取出lazy对象，当你在one-to-many的时候，设置lazy=true时，取对象的时候会使用延迟加载。但是你可以使用left join fetch强制取出lazy对象
     @Query("from Movie m " +
             "left outer join fetch m.country " +
             "left outer join fetch m.movieStatus " +
