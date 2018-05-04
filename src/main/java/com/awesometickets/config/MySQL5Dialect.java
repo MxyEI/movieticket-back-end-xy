2b5280fef5ed6312d84ba4b2c78a6c2e29f22572
MxyEI <1486157956@qq.com>
2018-05-04 12:54:54 +0800

    方言 定义数据库统一标准

--- src/main/java/com/awesometickets/config/MySQL5Dialect.java
+++ src/main/java/com/awesometickets/config/MySQL5Dialect.java
@@ -2,7 +2,9 @@ package com.awesometickets.config;
 
 import org.hibernate.dialect.MySQL5InnoDBDialect;
 
-
+/**
+ * 数据库统一标准
+ */
 public class MySQL5Dialect extends MySQL5InnoDBDialect {
 
     @Override
